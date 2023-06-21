7d1f08d868c92a2d450d8f02ed6471c6e7e0bc9d
aaqib-yfz <aaqibshaheer2001@gmail.com>
2023-06-21 14:06:03 +0500

    first

---
+++ sales.R
@@ -0,0 +1,22 @@
+#print("hello world")
+#View(penguins)
+#ggplot(data = penguins , aes(x = flipper_length_mm, y= body_mass_g)) + geom_point(aes(color=species))
+
+##first_var  <- 10
+
+##print(first_var)
+
+#ec1 <- c(13,28.5,71,10)
+#print(vec1)
+
+#a <- c(12,14,5,78,8)
+#print(a)
+
+# first calculation
+
+quarter1_sales <- 3567.98
+quarter2_sales <- 43810.67
+
+midyear_sales <- quarter1_sales + quarter2_sales
+year_sales <- midyear_sales*2
+
