c2de709787af0ef8710806f7c747c14e0d377c53
aaqib-yfz <aaqibshaheer2001@gmail.com>
2023-06-25 17:48:34 +0500

    pipes

--- Playground/ToothGrowthExploration.R
+++ Playground/ToothGrowthExploration.R
@@ -1,15 +1,18 @@
-data("ToothGrowth")
-View(ToothGrowth)
-filter_tg <- filter(ToothGrowth,dose==0.5)
-View(filter_tg)
-arrange(filter_tg,len)
+#data("ToothGrowth")
+#View(ToothGrowth)
+#filter_tg <- filter(ToothGrowth,dose==0.5)
+#View(filter_tg)
+#arrange(filter_tg,len)
 
 #nested but same result as above
-arrange(filter(ToothGrowth,dose == 0.5))
+#arrange(filter(ToothGrowth,dose == 0.5))
 
 
 filtered_tg <- ToothGrowth %>% 
     filter(dose== 0.5) %>% 
     arrange(len)
-    
+
+
+View(filtered_tg)    
+
 
