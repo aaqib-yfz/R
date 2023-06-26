f7bf6569b2d638b67f2d31b26d64f0ac1ba73d68
aaqib-yfz <aaqibshaheer2001@gmail.com>
2023-06-27 01:47:22 +0500

    pengu

--- Playground/penguins.R
+++ Playground/penguins.R
@@ -1,9 +1,41 @@
-penguins %>% 
-    select(-species)
+# penguins %>%
+#     select(-species)
+# 
+# penguins %>%
+#     rename(island_new = island)
+# 
+# rename_with(penguins,toupper)
+# 
+# clean_names(penguins)
+
+View(penguins)
+penguins %>% arrange(bill_length_mm)
+
+penguins2 <- penguins %>%  arrange(-bill_length_mm)
+View(penguins2)
+
+
+# penguins %>% 
+#     group_by(island) %>% 
+#     drop_na() %>% 
+#     summarize(mean_bill_mm = mean(bill_length_mm))
+
+# penguins %>% 
+#     group_by(island) %>% 
+#     drop_na() %>% 
+#     summarize(max_bill = max(bill_length_mm))
+#             
+
 
 penguins %>% 
-    rename(island_new = island)
+    group_by(island,species) %>% 
+    drop_na() %>% 
+    summarize( max_bl = max(bill_length_mm), mean_bl = mean(bill_length_mm))
 
-rename_with(penguins,toupper)
 
-clean_names(penguins)
+penguins %>% 
+    filter(species == 'Adelie') %>% 
+    drop_na()
+
+
+
